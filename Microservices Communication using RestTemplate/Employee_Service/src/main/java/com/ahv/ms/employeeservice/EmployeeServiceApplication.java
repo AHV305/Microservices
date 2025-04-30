015b569c40e19d4d946e52713ccf522da60c6a18
HarshaVardhan_A <harshavardhan.angadala@gmail.com>
2025-04-30 13:44:51 +0530

    Microservices Communication using RestTemplate

---
+++ Microservices Communication using RestTemplate/Employee_Service/src/main/java/com/ahv/ms/employeeservice/EmployeeServiceApplication.java
@@ -0,0 +1,23 @@
+package com.ahv.ms.employeeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableFeignClients
+public class EmployeeServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
